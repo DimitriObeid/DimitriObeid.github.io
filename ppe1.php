d33001884f1dee538365d03a08dbf220ddffdb42
DimitriObeid <obeiddimitri@gmail.com>
2021-06-13 00:57:15 +0200

    First push

---
+++ ppe1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+    <?php include_once("Header.php"); ?>
+    
+    <head>
+    </head>
+    
+    <body>
+        <div class="body-div">
+            <h1>PPE 1</h1>
+            <h2>Contexte</h2>
+            <p>L’entreprise GSB commercialise des produits médicaux à destination des professionnels de santé. Plusieurs commerciaux, des « Visiteurs médicaux » vont au contact desclients et prospects.
+
+            Afin d’être plus réactif et compétitif, l’entreprise souhaite développer l’administration de son Système d’Informations et les outils métiers.</p>
+            
+            <h2>Réalisation</h2>
+            <p> Le projet a été réalisé de novembre 2019 à février 2020. J'ai réalisé la partie de sauvegarde des données et déploiement simultané de l'image disque sur la centaine d'ordinateurs avec le logiciel Clonezilla.</p>
+            
+            <h2>Compétences mises en œuvre</h2>
+            <p>A1.1.1 : Analyse du cahier des charges d'un service à produire</p>
+            <p>A1.4.1 : Participation à un projet</p>
+            <p>A4.1.10 :  Rédaction d'une documentation d'utilisation</p>
+            
+            <h2>Productions associées</h2>
+            <p><a href="docs/PPE1/Installation de Clonezilla.pdf">Installation de Clonezilla</a></p>
+        </div>
+    </body>
+</html>
---
+++ ppe2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+    <?php include_once("Header.php"); ?>
+    
+    <head>
+    </head>
+    
+    <body>
+        <div class="body-div">
+            <h1>PPE 2</h1>
+            <h2>Contexte</h2>
+            <p>L'objectif de ce projet était de nous entraîner à concevoir un site web, et d'apprendre à se servir de Git</p>
+            
+            <h2>Réalisation</h2>
+            <p> Le projet a été réalisé de mars à juin 2020. J'ai réalisé la rédaction de la documentation d'utilisation de Git, ainsi que de nombreux tests avec ce logiciel.</p>
+            
+            <h2>Compétences mises en œuvre</h2>
+            <p>A1.1.1 : Analyse du cahier des charges d'un service à produire</p>
+            <p>A1.4.1 : Participation à un projet</p>
+            <p>A4.1.1 : Proposition d'une solution applicative </p>
+            <p>A4.1.8 : Réalisation des tests nécessaires à la validation d'éléments adaptés ou développés</p>
+            <p>A4.1.9 : Rédaction d'une documentation technique</p>
+            <p>A4.1.10 : Rédaction d'une documentation d'utilisation</p>
+            <p>A5.2.4 : Étude d'une technologie, d'un composant, d'un outil ou d'une méthode</p>
+            
+            <h2>Productions associées</h2>
+            <p><a href="docs/PPE2/Documentation d'utilisation de Git.pdf">Documentation d'utilisation de Git</a></p>
+        </div>
+    </body>
+</html>
